d9ef3d80ec5f93d3f900515ef067aef6ab9b1f7a
Norbert <norbert.swieconek@spartavity.com>
2026-03-13 10:55:13 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/pl/swieconek/transaction_aggregator/TransactionAggregatorApplication.java
@@ -0,0 +1,13 @@
+package pl.swieconek.transaction_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/swieconek/transaction_aggregator/TransactionAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.swieconek.transaction_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

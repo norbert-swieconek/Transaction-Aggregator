af240e94ad1a45b2f0c7730f23ea6932bcd36c62
Norbert <norbert.swieconek@spartavity.com>
2026-03-13 15:59:12 +0100

    Implement asynchronous requests and caching

--- src/main/java/pl/swieconek/transaction_aggregator/TransactionAggregatorApplication.java
+++ src/main/java/pl/swieconek/transaction_aggregator/TransactionAggregatorApplication.java
@@ -2,7 +2,9 @@ package pl.swieconek.transaction_aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class TransactionAggregatorApplication {
 

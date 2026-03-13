799a91ff79467110ca11099a90188e932030b0df
Norbert <norbert.swieconek@spartavity.com>
2026-03-13 15:29:45 +0100

    Add retry mechanism for handling 503 and 529 server errors

--- src/main/java/pl/swieconek/transaction_aggregator/service/TransactionService.java
+++ src/main/java/pl/swieconek/transaction_aggregator/service/TransactionService.java
@@ -2,6 +2,7 @@ package pl.swieconek.transaction_aggregator.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientResponseException;
 import pl.swieconek.transaction_aggregator.dto.TransactionDto;
 
 import java.util.Arrays;
@@ -18,18 +19,33 @@ public class TransactionService {
     }
 
     public List<TransactionDto> getTransactions(String account) {
-        TransactionDto[] response1 = restClient.get()
-                .uri("http://localhost:8888/transactions?account=" + account)
-                .retrieve()
-                .body(TransactionDto[].class);
-
-        TransactionDto[] response2 = restClient.get()
-                .uri("http://localhost:8889/transactions?account=" + account)
-                .retrieve()
-                .body(TransactionDto[].class);
+        TransactionDto[] response1 = fetchFromServer("http://localhost:8888/transactions?account=" + account);
+        TransactionDto[] response2 = fetchFromServer("http://localhost:8889/transactions?account=" + account);
 
         return Stream.concat(Arrays.stream(response1), Arrays.stream(response2))
                 .sorted(Comparator.comparing(TransactionDto::timestamp).reversed())
                 .toList();
+    }
+
+    private TransactionDto[] fetchFromServer(String url) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                return restClient.get()
+                        .uri(url)
+                        .retrieve()
+                        .body(TransactionDto[].class);
+            } catch (RestClientResponseException e) {
+                int code = e.getStatusCode().value();
+
+                if (code == 503 || code == 529) {
+                    if (i == 4) {
+                        throw e;
+                    }
+                } else {
+                    throw e;
+                }
+            }
+        }
+        throw new IllegalStateException();
     }
 }
